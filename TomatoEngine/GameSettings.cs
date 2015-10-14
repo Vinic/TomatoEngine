b2908b99b6930725f84dc104453a1f0d727b27e0
Rick Wesselink <rickwesselink1@hotmail.com>
2015-10-14 19:17:18 +0200

    Init van de GameSettings.cs

---
+++ TomatoEngine/GameSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TomatoEngine
+{
+    class GameSettings
+    {
+        private float _fSpeed;
+        private bool[] _buttons;
+
+		public GameSettings(float fSpeed, bool[] buttons)
+        {
+            _fSpeed = fSpeed;
+            _buttons = buttons;
+        }
+
+		public void GamePause(bool Pause)
+        {
+            if (Pause)
+            {
+				// Iets zoals Game.Pause();
+            }
+        }
+    }
+}
