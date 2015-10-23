108c0072d92af6a4fb7e9f245da55d843eb955ac
Marnick <marnick.hartgers@gmail.com>
2015-10-23 18:11:16 +0200

    Background test

--- TomatoEngine/Levels.cs
+++ TomatoEngine/Levels.cs
@@ -8,7 +8,13 @@ namespace TomatoEngine
     static class Levels
     {
         public static void SpaceTest(TomatoMainEngine engine){
+            //adds the background
+            RenderObject background = new RenderObject(0,0,30,30);
+            background.SetTexture(ResourceManager.GetTexture("background"));
+            TomatoMainEngine.GameObjects.Add(background);
+            //add a space ship
             TomatoMainEngine.GameObjects.Add(new SpaceGame.SpaceShip());
+
         }
     }
 }
